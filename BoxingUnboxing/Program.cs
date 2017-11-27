f3437c2dda1748e773158e65f3064e3952efbe33
amalfushi <amalfushi@gmail.com>
2017-11-27 15:35:04 -0800

    finished Boxing/Unboxing assignemnt

---
+++ BoxingUnboxing/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> boxy = new List<object>();
+            boxy.Add(7);
+            boxy.Add(28);
+            boxy.Add(-1);
+            boxy.Add(true);
+            boxy.Add("Chair");
+
+            int sum = 0;
+            foreach(object thing in boxy){
+                if(thing is int){
+                    sum += (int)thing;
+                }
+
+                System.Console.WriteLine(thing);
+                
+            }
+            System.Console.WriteLine("The sum of the integers is : {0}", sum);
+        }
+    }
+}
