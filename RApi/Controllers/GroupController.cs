8817ccef4ae0468faa117c6fef0da3aed23c543f
amalfushi <amalfushi@gmail.com>
2017-12-01 12:18:45 -0800

    addid .Join to groups

--- RApi/Controllers/GroupController.cs
+++ RApi/Controllers/GroupController.cs
@@ -6,8 +6,10 @@ using JsonData;
 namespace MusicApi.Controllers {
     public class GroupController : Controller {
         List<Group> allGroups {get; set;}
+        List<Artist> allArtists { get; set;}
         public GroupController() {
             allGroups = JsonToFile<Group>.ReadJson();
+            allArtists = JsonToFile<Artist>.ReadJson();
         }
 
         [Route("groups")]
@@ -17,19 +19,28 @@ namespace MusicApi.Controllers {
             return Json(allGroups);
         }
 
-        [Route("groups/{name}")]
+        [Route("groups/{name}/{listArtists}")]
         [HttpGet]
-        public JsonResult OneGroup(string name)
+        public JsonResult OneGroup(string name, bool listArtists)
         {
-            return Json(allGroups.FindAll(g => g.GroupName == name));
+            List<Group> oneGroup = allGroups.FindAll(g => g.GroupName == name).ToList();
+            if(listArtists)
+            {
+                oneGroup = oneGroup.GroupJoin(allArtists, g => g.Id, a => a.GroupId, (g, a)=> {g.Members = a.ToList(); return g;}).ToList();
+            }
+            return Json(oneGroup);
         }
 
-        [Route("groups/id/{id}")]
+        [Route("groups/id/{id}/{listArtists}")]
         [HttpGet]
-        public JsonResult AllGroupsFromID(int id)
+        public JsonResult AllGroupsFromID(int id, bool listArtists)
         {
-            // .Join(Groups, a => a.GroupId, g => g.Id, (a, g)=>{ a.Group = g; return a; })
-            return Json(allGroups.FindAll(g => g.Id == id));
+            List<Group> oneGroup = allGroups.FindAll(g => g.Id == id).ToList();
+            if(listArtists)
+            {
+                oneGroup = oneGroup.GroupJoin(allArtists, g => g.Id, a => a.GroupId, (g, a)=> {g.Members = a.ToList(); return g;}).ToList();
+            }
+            return Json(oneGroup);
         }
     }
 }
