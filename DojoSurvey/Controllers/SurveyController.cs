c6fce15653e50c5293379d9c08ecf55fe70d3ddd
amalfushi <amalfushi@gmail.com>
2017-12-04 10:45:59 -0800

    finished Dojo Survey Assignment

---
+++ DojoSurvey/Controllers/SurveyController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DojoSurvey
+{
+    public class Survey : Controller{
+        [Route("")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.errors = new string[0];
+            return View();
+        }
+
+        [Route("submit")]
+        [HttpPost]
+        public IActionResult Submit(string name, string location, string language, string comment)
+        {
+            if(name != null)
+            {
+                ViewBag.name = name;
+                ViewBag.location = location;
+                ViewBag.language = language;
+                ViewBag.comment = comment;
+                return View("Results");
+            }
+            ViewBag.errors = new string[]{"Names are required."};
+            return View("Index");
+
+            
+        }
+    }
+}
